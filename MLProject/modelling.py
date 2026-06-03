2b5398d7ecf73f721703d9cb9f2c586cb31ed5d1
journey835 <journey.me835@gmail.com>
2026-06-03 15:01:36 +0700

    complete workflow and docker push automation

---
+++ MLProject/modelling.py
@@ -0,0 +1,29 @@
+import os
+import argparse
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestClassifier
+
+def train_base_model(data_dir):
+    print(f"Membaca data dari path: {data_dir}")
+    X_train = pd.read_csv(os.path.join(data_dir, "X_train_ready.csv"))
+    X_test = pd.read_csv(os.path.join(data_dir, "X_test_ready.csv"))
+    y_train = pd.read_csv(os.path.join(data_dir, "y_train.csv")).values.ravel()
+    y_test = pd.read_csv(os.path.join(data_dir, "y_test.csv")).values.ravel()
+    
+    mlflow.set_tracking_uri(os.environ.get("MLFLOW_TRACKING_URI", "runs:/"))
+    mlflow.set_experiment("Digital_Marketing_Campaign_CI")
+    mlflow.sklearn.autolog()
+    
+    with mlflow.start_run(run_name="CI_Docker_Execution"):
+        model = RandomForestClassifier(random_state=42)
+        model.fit(X_train, y_train)
+        print("Model berhasil dilatih di dalam Docker Container!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", type=str, default="digital_marketing_campaign_dataset_preprocessing")
+    args = parser.parse_args()
+    
+    train_base_model(data_dir=args.data_dir)
